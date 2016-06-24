5fffa724194e7b60b5490ba285d4408447e21e1f
hoaixuimay <hoaixuimay@gmail.com>
2016-06-24 10:00:43 +0700

    Update for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,29 @@ module.exports = function(grunt) {
             colors: true,
             bootstrap: 'vendor/autoload.php'
         }
+    },
+    
+    karma: {
+        unit: {
+            configFile: 'karma.conf.js',
+            autoWatch: true
+        }
     }
   });
 
   // Load the plugin that provides the "phpunit" task.
   grunt.loadNpmTasks('grunt-phpunit');
 
+  // Karma
+  grunt.loadNpmTasks('grunt-karma');
+
   // Default task(s).
-  grunt.registerTask('default', ['phpunit']);
+  grunt.registerTask('default', ['phpunit','karma']);
+  
+  // Distributed task(s).
+  grunt.registerTask('test', ['phpunit','karma']);
+  
+  // Distributed task(s).
+  grunt.registerTask('dist', ['phpunit','karma']);
 
 };
